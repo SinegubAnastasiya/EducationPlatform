69cd670c329d7dc2704a92e2dda474a18e3f09dd
SinegubAnastasiya <fedukevich98@mail.ru>
2024-03-23 14:43:39 +0300

    fixed

--- src/tests/controller/course.controller.test.ts
+++ src/tests/controller/course.controller.test.ts
@@ -1,7 +1,19 @@
 import supertest from 'supertest';
 import { app } from '../../app';
 
-let id;
+let id: number;
+
+describe('POST course', () => {
+  test('Get success', async () => {
+    const res = await supertest(app).post('/course/').send({ course: 'course2', description: 'descr2' });
+
+    id = res.body[0].id;
+    expect(res.statusCode).toBe(200);
+    expect(res.body[0].course).toBe('course2');
+    expect(res.body[0].description).toBe('descr2');
+    expect(res.body.length).toBeGreaterThanOrEqual(1);
+  });
+});
 
 describe('GET course', () => {
   test('Get success', async () => {
@@ -14,47 +26,35 @@ describe('GET course', () => {
 
 describe('GET course', () => {
   test('Get success', async () => {
-    const res = await supertest(app).get('/course/1');
+    const res = await supertest(app).get(`/course/${id}`);
 
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBeGreaterThanOrEqual(1);
   });
 });
 
-// describe('POST course', () => {
-//   test('Get success', async () => {
-//     const res = await supertest(app).post('/course/').send({ course: 'course2', description: 'descr2' });
+describe('PUT course', () => {
+  test('Get success', async () => {
+    const res = await supertest(app).put(`/course/${id}`).send({ course: 'course6', description: 'descr6' });
 
-// id = result.body[0].id
-//     expect(res.statusCode).toBe(200);
-//     expect(res.body[0].course).toBe('course2');
-//     expect(res.body[0].description).toBe('descr2');
-//     expect(res.body.length).toBeGreaterThanOrEqual(1);
-//   });
-// });
+    expect(res.statusCode).toBe(200);
+    expect(res.body[0].course).toBe('course6');
+    expect(res.body[0].description).toBe('descr6');
+    expect(res.body.length).toBeGreaterThanOrEqual(1);
 
-// describe('PUT course', () => {
-//   test('Get success', async () => {
-//     const res = await supertest(app).put('/course/${ id }').send({ course: 'course6', description: 'descr6' });
+    console.log(res.body);
+  });
+});
 
-//     expect(res.statusCode).toBe(200);
-//     expect(res.body[0].course).toBe('course6');
-//     expect(res.body[0].description).toBe('descr6');
-//     expect(res.body.length).toBeGreaterThanOrEqual(1);
+describe('DELETE course', () => {
+  test('Get success', async () => {
+    const res = await supertest(app).delete(`/course/${id}`);
 
-//     console.log(res.body);
-//   });
-// });
+    expect(res.statusCode).toBe(200);
+    expect(res.body[0].course).toBe('course5');
+    expect(res.body[0].description).toBe('descr5');
+    expect(res.body.length).toBeGreaterThanOrEqual(1);
 
-// describe('DELETE course', () => {
-//   test('Get success', async () => {
-//     const res = await supertest(app).delete('/course/5');
-
-//     expect(res.statusCode).toBe(200);
-//     expect(res.body[0].course).toBe('course5');
-//     expect(res.body[0].description).toBe('descr5');
-//     expect(res.body.length).toBeGreaterThanOrEqual(1);
-
-//     console.log(res.body);
-//   });
-// });
+    console.log(res.body);
+  });
+});
