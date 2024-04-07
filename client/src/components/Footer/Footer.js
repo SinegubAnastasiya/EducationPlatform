7f1ee6448358aa9e6f922640ef7b20b02ca0b36a
SinegubAnastasiya <fedukevich98@mail.ru>
2024-04-07 20:42:57 +0300

    finished page Courses

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,36 +2,38 @@ import style from './footer.module.css'
 
 function Footer() {
     return (
-        <div className={style.footer}>
-            <div className={style.navigation}>
-                <div className={style.left_side}>
-                    <p>Home</p>
-                    <p>Textbook</p>
-                    <p>Statistics</p>
-                    <p>Sprint</p>
+        <div className={style.section_footer}>
+            <div className={style.footer}>
+                <div className={style.navigation}>
+                    <div className={style.left_side}>
+                        <p>Home</p>
+                        <p>Textbook</p>
+                        <p>Statistics</p>
+                        <p>Sprint</p>
+                    </div>
+
+                    <div className={style.right_side}>
+                        <p>Alex</p>
+                        <p>Gabriel</p>
+                        <p>Marcus</p>
+                    </div>
                 </div>
 
-                <div className={style.right_side}>
-                    <p>Alex</p>
-                    <p>Gabriel</p>
-                    <p>Marcus</p>
+                <div className={style.line}></div>
+
+                <div className={style.icons}>
+                    <div className={style.left_icon}>
+                        <div className={style.face}></div>
+                        <div className={style.img_GT}></div>
+                        <div className={style.youtube}></div>
+                    </div>
+
+                    <p>©2021 Hschool. Project for Hschool.</p>
+
                 </div>
-            </div>
-
-            <div className={style.line}></div>
-
-            <div className={style.icons}>
-                <div className={style.left_icon}>
-                    <div className = {style.face}></div>
-                    <div className = {style.img_GT}></div>
-                    <div className = {style.youtube}></div>
-                </div>
-
-                <p>©2021 Hschool. Project for Hschool.</p>
-
             </div>
         </div>
-)
+    )
 }
 
 export default Footer
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,11 +2,13 @@ import style from './header.module.css'
 
 function Header() {
     return (
-        <div className={style.header}>
-            <p>Hschool</p>
-            <div>
-                <button className={style.btn_log}>Login →</button>
-                <button className={style.btn_sign}>Sign Up</button>
+        <div className={style.section_1}>
+            <div className={style.header}>
+                <p>Hschool</p>
+                <div>
+                    <button className={style.btn_log}>Login →</button>
+                    <button className={style.btn_sign}>Sign Up</button>
+                </div>
             </div>
         </div>
     )
