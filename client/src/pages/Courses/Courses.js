0853704cb5b69162ada31e07326ba25667f9bc2c
SinegubAnastasiya <fedukevich98@mail.ru>
2024-04-28 20:29:00 +0300

    fixed Courses

--- client/src/pages/Courses/Courses.js
+++ client/src/pages/Courses/Courses.js
@@ -3,6 +3,15 @@ import Header from "../../components/Header/Header"
 import style from "./courses.module.css"
 
 function Courses() {
+    const sections = [
+        { course: 'Javascript', description: 'JavaScript is a practical course where students learn the basics of JavaScript. It covers variables, operators, conditionals, loops, functions, and data manipulation.', 
+        image: style.image_1 }, 
+        { course: 'Typescript', description: 'TypeScript is a course that provides an introduction to TypeScript. Students will learn about TypeScript\'s key features, such as type annotations, interfaces, classes, and modules', 
+        image: style.image_2 },
+        { course: 'Python', description: 'Students will learn about variables, data types, conditionals, loops, functions, and file handling. Through hands-on exercises and projects, students will gain proficiency in writing Python code and solving real-world problems.', 
+        image: style.image_3 }
+    ]
+    
     return <div>
         <Header />
         <div className={style.background}>
@@ -14,32 +23,15 @@ function Courses() {
                 </div>
             </div>
 
-            <div className={style.section}>
-                <div className={style.image_1}></div>
+            {sections.map((el, index) => <div key = {index} className={style.section}>
+                <div className={el.image}></div>
                 <div>
-                    <h1>JavaScript</h1>
+                    <h1>{el.course}</h1>
                     <div className={style.baseline}></div>
-                    <p>JavaScript is a practical course where students learn the basics of JavaScript. It covers variables, operators, conditionals, loops, functions, and data manipulation.</p>
+                    <p>{el.description}</p>
                 </div>
-            </div>
+            </div>)}
 
-            <div className={style.section}>
-                <div className={style.image_2}></div>
-                <div>
-                    <h1>TypeScript</h1>
-                    <div className={style.baseline}></div>
-                    <p>TypeScript is a course that provides an introduction to TypeScript. Students will learn about TypeScript's key features, such as type annotations, interfaces, classes, and modules</p>
-                </div>
-            </div>
-
-            <div className={style.section}>
-            <div className={style.image_3}></div>
-                <div>
-                    <h1>Python</h1>
-                    <div className={style.baseline}></div>
-                    <p>Students will learn about variables, data types, conditionals, loops, functions, and file handling. Through hands-on exercises and projects, students will gain proficiency in writing Python code and solving real-world problems.</p>
-                </div>
-            </div>
         </div>
 
         <Footer />
